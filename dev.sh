69c9e6e986da8f4077ab89b3719d0a4ec14e8a82
Gaetan Semet <gaetan@xeberon.net>
2017-05-31 17:02:01 +0200

    pipenv install can create env
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- dev.sh
+++ dev.sh
@@ -4,9 +4,8 @@ if [[ $1 == "install" ]]; then
     curl -L https://github.com/ebergama/sonarr-sub-downloader/archive/v0.4.zip \
             -o sonarr-sub-downloader.zip
     unzip -f sonarr-sub-downloader.zip
-    pipenv --three
+    pipenv install --dev --three
     pipenv lock -r
-    pipenv install
 fi
 
 
