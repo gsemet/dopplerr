2166e6ff71595178b94223a453a76fa9a17bc9b1
Gaetan Semet <gaetan@xeberon.net>
2017-05-31 17:58:32 +0200

    install log

--- dev.sh
+++ dev.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 if [[ $1 == "install" ]]; then
+    echo "Downloading sonarr sub downloader"
     curl -L https://github.com/ebergama/sonarr-sub-downloader/archive/v0.4.zip \
             -o sonarr-sub-downloader.zip
     unzip -f sonarr-sub-downloader.zip
+    echo "Setting up my environment..."
     pipenv install --dev --three
     pipenv lock -r
+    echo "Installing subdlsrv"
     python setup.py develop
 fi
 
