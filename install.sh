aa28f31e15db17981004a37c7d9f77fbaa2290be
Gaetan Semet <gaetan@xeberon.net>
2017-06-03 14:08:51 +0200

    ignore sonarr-sub-downloader
    no more needed
    
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- install.sh
+++ install.sh
@@ -4,11 +4,6 @@ set -e
 
 cd $(dirname $0)
 
-echo "Downloading sonarr sub downloader"
-rm -rvf sonarr-sub-downloader*
-curl -L https://github.com/ebergama/sonarr-sub-downloader/archive/v0.4.zip \
-        -o sonarr-sub-downloader.zip
-unzip sonarr-sub-downloader.zip
 echo "Setting up my environment..."
 VERSION_ARGS="--three"
 if [[ $TRAVIS_PYTHON_VERSION == '2.7' ]]; then
