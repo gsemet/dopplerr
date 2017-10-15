67227d14049ff730f4939d81473a2d0bebc5bb5b
Gaetan Semet <gaetan@xeberon.net>
2017-10-15 15:23:21 +0200

    freeze version of pipenv
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- bootstrap-system.sh
+++ bootstrap-system.sh
@@ -14,5 +14,5 @@ else
 fi
 
 # Freeze the version of pip and pipenv for setup reproductibility
-pip install -U 'pip==9.0' 'pipenv==4.1' 'setuptools!=36.0.0' || echo "you may need to sudo me !"
+pip install -U 'pip==9.0' 'pipenv==4.1.4' 'setuptools!=36.0.0' || echo "you may need to sudo me !"
 echo "Done"
