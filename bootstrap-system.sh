947db3bac3f0616f91ba9d64712c7adba173f15b
Gaetan Semet <gaetan.semet@renault.com>
2017-10-16 16:06:59 +0200

    split bootstrap scripts

--- bootstrap-system.sh
+++ bootstrap-system.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-echo "Bootstrap system tools"
+echo "Bootstrap system dependencies"
 echo "sudo it accodingly to your system"
+# git              : For PBR
+# make             : Developer's toolbox
+# pandoc           : for README .md =>.rst
+# libpython3.6-dev : for 'Python.h' (if Twisted wheel rebuild is needed)
 
 if [ -f /etc/debian_version ]; then
-    apt-get install git make pandoc
+    apt-get -y install \
+               git \
+               make \
+               pandoc \
+               libpython3.6-dev
 elif [ -f /etc/redhat-release ]; then
-    yum install git make
+    yum install git make pandoc
+elif [[ -f /etc/os-release && $(grep "alpine" /etc/os-release) != "" ]]; then
+    echo "Alpine dependencies should be described in Dockerfile"
 # elif [ -f /etc/??? ]; then
 #     brew install pip pipenv make git pandoc
 else
-    echo "Please ensure 'git', 'make', and 'pandoc' are installed on your system"
+    echo "Please ensure 'git', 'make', and 'pandoc' and 'Python.h' headers are installed on your system"
 fi
-
-# Freeze the version of pip and pipenv for setup reproductibility
-pip install -U 'pip==9.0' 'pipenv==4.1.4' 'setuptools!=36.0.0' || echo "you may need to sudo me !"
 echo "Done"
