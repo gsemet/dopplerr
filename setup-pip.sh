f57150db58cfd84840e8b8751065d131b124ddd9
Gaetan Semet <gaetan@xeberon.net>
2017-10-18 00:19:34 +0200

    freezen setuptools 36.6 and pipenv 8.2.7 in Dockerfile
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- setup-pip.sh
+++ setup-pip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Freeze the version of pip and pipenv for setup reproductibility
-pip install -U 'pip==9.0.1' 'pipenv==7' 'setuptools!=36.0.0' || echo "you may need to sudo me !"
+pip install -U 'pip==9.0.1' 'pipenv==8.2.7' 'setuptools==36.6.0' || echo "you may need to sudo me !"
 echo "Done"
