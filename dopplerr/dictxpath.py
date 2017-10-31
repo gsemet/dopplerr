5fbd51982ac10aa977bc795655c0b6137042aaec
Gaetan Semet <gaetan@xeberon.net>
2017-10-31 17:30:28 +0100

    pylint
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- dopplerr/dictxpath.py
+++ dopplerr/dictxpath.py
@@ -9,6 +9,7 @@ import re
 # tells flake8 to ignore complexity check for this file
 # flake8: noqa
 
+
 def get_node_by_xpath(mapping, xpath, default=None, ignore_errors=False,
                       handle_list_selector=False):
     '''Return the node pointed to by xpath from mapping.
