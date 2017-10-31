cfe2e25cce96f505bfa22818349514d6369b823c
Gaetan Semet <gaetan@xeberon.net>
2017-10-31 09:52:18 +0100

    retrain pylint
    enable naming convention
    
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- dopplerr/tests/test_dopplerr.py
+++ dopplerr/tests/test_dopplerr.py
@@ -9,11 +9,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-# import pytest
-#
 from dopplerr.downloader import DopplerrDownloader
-
-# from dopplerr import dopplerr
 
 
 class Testdopplerr(object):
