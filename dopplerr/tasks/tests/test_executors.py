9a85a37d134b5321792a280d2e86d5d5edf41e81
Gaetan Semet <gaetan@xeberon.net>
2017-11-04 11:30:50 +0100

    using asynctest.TestCase
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- dopplerr/tasks/tests/test_executors.py
+++ dopplerr/tasks/tests/test_executors.py
@@ -1,12 +1,15 @@
 # coding: utf-8
 
 import asyncio
+import logging
 import time
 
-import pytest
+from asynctest import TestCase
 
 from dopplerr.tasks.manager import DopplerrTasksManager
 from dopplerr.tasks.threaded import ThreadedTask
+
+log = logging.getLogger(__name__)
 
 
 class SingleExecutor(ThreadedTask):
@@ -19,39 +22,39 @@ class SingleExecutor(ThreadedTask):
         raise NotImplementedError
 
 
-@pytest.mark.asyncio
-async def test_executors():
-    print("")
-    de = DopplerrTasksManager()
+class TestApscheduler(TestCase):
+    async def test_executors(self):
+        log.info("")
+        de = DopplerrTasksManager()
 
-    def mkprefix(i):
-        return " " * 15 + " " * 45 * i
+        def mkprefix(i):
+            return " " * 15 + " " * 45 * i
 
-    def long_sync_task(i):
-        # all tasks should run inside the SAME thread
-        prefix = mkprefix(1)
-        print(prefix + "long  sync task {}: begin".format(i))
-        for _ in range(0, i * 10):
-            print(prefix + "long  sync task {}: working".format(i))
-            # note: time.sleep does NOT block current thread, so other "blocking" task
-            # might actually overlap their executions
-            time.sleep(0.1)
-        print(prefix + "long  sync task {}: end".format(i))
+        def long_sync_task(i):
+            # all tasks should run inside the SAME thread
+            prefix = mkprefix(1)
+            log.info(prefix + "long  sync task %s: begin", i)
+            for _ in range(0, i * 10):
+                log.info(prefix + "long  sync task %s: working", i)
+                # note: time.sleep does NOT block current thread, so other "blocking" task
+                # might actually overlap their executions
+                time.sleep(0.1)
+            log.info(prefix + "long  sync task %s: end", i)
 
-    async def long_async_task(i):
-        print("long async task {}: begin".format(i))
-        await SingleExecutor().execute(long_sync_task, i)
-        print("long async task {}: more work after results from run in thread task".format(i))
-        await asyncio.sleep(1)
-        print("long async task {}: end".format(i))
+        async def long_async_task(i):
+            log.info("long async task %s: begin", i)
+            await SingleExecutor().execute(long_sync_task, i)
+            log.info("long async task %s: more work after results from run in thread task", i)
+            await asyncio.sleep(1)
+            log.info("long async task %s: end", i)
 
-    print("event loop thread" + " " * 40 + "worker thread")
-    de.post_task(long_async_task(1))
-    de.post_task(long_async_task(2))
-    de.post_task(long_async_task(3))
-    de.post_task(long_async_task(4))
-    print("waiting 5s")
-    for _ in range(0, 60):
-        print("-- running long async tasks: {} --".format(de.status()['background_tasks']))
-        await asyncio.sleep(0.1)
-    print("end of unittest")
+        log.info("event loop thread" + " " * 40 + "worker thread")
+        de.post_task(long_async_task(1))
+        de.post_task(long_async_task(2))
+        de.post_task(long_async_task(3))
+        de.post_task(long_async_task(4))
+        log.info("waiting 5s")
+        for _ in range(0, 60):
+            log.info("-- running long async tasks: %s --", de.status()['background_tasks'])
+            await asyncio.sleep(0.1)
+        log.info("end of unittest")
