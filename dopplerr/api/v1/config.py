71bca34852fe7716aef5204288a211de878b969e
Gaetan Semet <gaetan@xeberon.net>
2017-11-05 21:56:01 +0100

    split config rest api
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

---
+++ dopplerr/api/v1/config.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+import logging
+
+from sanic import Blueprint
+from sanic_transmute import add_route
+from sanic_transmute import describe
+from schematics.models import Model
+from schematics.types import StringType
+
+from dopplerr.config import DopplerrConfig
+
+log = logging.getLogger(__name__)
+
+
+class ConfigDir(Model):
+    configdir = StringType()
+    basedir = StringType()
+    appdir = StringType()
+    frontenddir = StringType()
+
+
+bp = Blueprint('config', url_prefix="/api/v1")
+
+
+@describe(paths="/config/general/dir")
+async def config_directories() -> ConfigDir:
+    return {
+        "configdir": DopplerrConfig().get_cfg_value("general.configdir"),
+        "basedir": DopplerrConfig().get_cfg_value("general.basedir"),
+        "appdir": DopplerrConfig().get_cfg_value("general.appdir"),
+        "frontenddir": DopplerrConfig().get_cfg_value("general.frontenddir"),
+    }
+
+
+add_route(bp, config_directories)
