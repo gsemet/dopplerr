6b98821a567008ce562f73db99d2ba2eb237ea3e
Gaetan Semet <gaetan@xeberon.net>
2017-11-05 22:15:21 +0100

    Status page
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- frontend/src/components/pages/status.vue
+++ frontend/src/components/pages/status.vue
@@ -1,24 +1,49 @@
 <template>
   <div>
-    Subtitle Download:
-    <span
-      v-if="subtitle_downloader === 1"
-    >
-      Active
-      <i
-        class="material-icons text-negative"
-        style="font-size: 22px">info
-      </i>
-    </span>
-    <span
-      v-else-if ="subtitle_downloader === 0"
-    >
-      Inactive
-      <i
-        class="material-icons text-positive"
-        style="font-size: 22px">info
-      </i>
-    </span>
+    <div>
+      <h4>Subtitle Download</h4>
+      <span
+        v-if="subtitle_downloader.active === 1"
+      >
+        Active
+        <i
+          class="material-icons text-negative"
+          style="font-size: 22px">info
+        </i>
+      </span>
+      <span
+        v-else-if ="subtitle_downloader.active === 0"
+      >
+        Inactive
+        <i
+          class="material-icons text-positive"
+          style="font-size: 22px">info
+        </i>
+      </span>
+    </div>
+    <div>
+      <h4>Disc Scanner</h4>
+      <span
+        v-if="disc_scanner.active === 1"
+      >
+        Active
+        <i
+          class="material-icons text-negative"
+          style="font-size: 22px">info
+        </i>
+      </span>
+      <span
+        v-else-if ="disc_scanner.active === 0"
+      >
+        Inactive
+        <i
+          class="material-icons text-positive"
+          style="font-size: 22px">info
+        </i>
+      </span><br>
+      <span>Interval: {{ disc_scanner.interval_sec }}s </span><br>
+      <span>Next run time: {{ disc_scanner.next_run_time }} </span>
+    </div>
   </div>
 </template>
 
@@ -27,7 +52,14 @@ export default {
   data () {
     return {
       timer: null,
-      subtitle_downloader: null,
+      subtitle_downloader: {
+        active: 0,
+      },
+      disc_scanner: {
+        active: 0,
+        next_run_time: null,
+        interval_sec: 0,
+      },
     }
   },
 
@@ -36,7 +68,10 @@ export default {
     fetchStatus: function () {
       this.axios.get('/api/v1/tasks/status/', {})
         .then(response => {
-          this.subtitle_downloader = response.data.subtitle_downloader.active
+          this.subtitle_downloader.active = response.data.subtitle_downloader.active
+          // this.disc_scanner.active = response.data.subtitle_downloader.active
+          this.disc_scanner.next_run_time = response.data.disc_scanner.next_run_time.substring(0, 19).replace('T', ' ')
+          this.disc_scanner.interval_sec = response.data.disc_scanner.interval_sec
         })
         .catch(error => {
           console.log(error)
