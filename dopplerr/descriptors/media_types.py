9dd671896210f3e2c6a9b60f7260cdccffd3c1dc
Gaetan Semet <gaetan@xeberon.net>
2017-11-19 01:18:38 +0100

    quick and dirty fix of glob search
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

---
+++ dopplerr/descriptors/media_types.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+VIDEO_FILES_EXT = [
+    'asf',
+    'avc',
+    'avi',
+    'divx',
+    'm4v',
+    'mkv',
+    'mov',
+    'mp4',
+    'mpg',
+    'ogv',
+    'qt',
+    'viv',
+    'vp3',
+    'wmv',
+]
